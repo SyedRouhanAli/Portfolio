c186331db375d9bbb72d0b84aa2d64df39dfab82
syed rouhan ali <59330813+SyedRouhanAli@users.noreply.github.com>
2022-12-03 01:58:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+.btn {
+    border: none;
+    background-color: inherit;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    display: inline-block;
+    text-align:center;
+    margin-top: auto ;
+    position: relative ;
+    text-align: right;
+    width: 300px;
+}
+
+
+.success {
+    color: green;
+    text-align: center;
+}
+
+.success:hover {
+    background-color: #04AA6D;
+    color: white;
+}
+
+
+.info {
+    color: dodgerblue;
+    text-align: center;
+}
+
+.info:hover {
+    background: #2196F3;
+    color: white;
+}
+
+
+.warning {
+    color: orange;
+    text-align: center;
+}
+
+.warning:hover {
+    background: #ff9800;
+    color: white;
+}
+
+
+.danger {
+    color: red;
+    text-align: center;
+}
+
+.danger:hover {
+    background: #f44336;
+    color: white;
+}
+
+
+.default {
+    color: black;
+    text-align: center;
+}
+
+.default:hover {
+    background: #e7e7e7;
+}
